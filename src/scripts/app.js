ead1cf0cca6956e4d1d8ec0f045beb912b85ef31
Elodie-st <elodie.somerlinck@student.heaj.be>
2026-08-09 22:45:41 +0200

    2 page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,27 @@
-console.info('Hello world');
+"use strict"
+//flash carte
+document.querySelectorAll('.flashCard__carte').forEach(carte => {
+    const card = carte.querySelector('.flashCard__inner');
+    card.addEventListener('click', () => {
+        card.classList.toggle('is-flipped');
+    });
+});
+
+//nav
+const menuToggle = document.querySelector(".menu__toggle");
+const menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu__link");
+
+if(menuToggle){
+    // click sur le bouton
+    menuToggle.addEventListener("click", menuOpen);
+
+    // click sur les liens 
+    for(let menuLink of menuLinks){
+        menuLink.addEventListener("click", menuOpen);
+    }
+}
+// function pour ouvrir et fermer le menu
+function menuOpen(){
+    menu.classList.toggle("menu--open");
+}
